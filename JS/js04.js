9953ec898fbac557e8566e064790c1e8d9faf042
thien0709 <thien070904@gmail.com>
2023-07-15 10:36:11 +0700

    update js04

---
+++ JS/js04.js
@@ -0,0 +1,15 @@
+let action = document.querySelector(".action")
+function checkedValue(){
+    let radio = document.getElementsByName("math")
+for(let i = 0 ;i<radio.length;i++){
+    if(radio[i].checked){
+        return radio[i].value
+    }
+}
+}
+action.addEventListener("click" , function(){
+    
+})
+let x1 = document.querySelector(".x1")
+let x2 = document.querySelector(".x2")
+
