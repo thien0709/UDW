2be1ba4c8059493771455c394d2f579777b8c5b0
thien0709 <thien070904@gmail.com>
2023-07-15 10:54:34 +0700

    done js04

--- JS/js04.js
+++ JS/js04.js
@@ -1,15 +1,36 @@
-let action = document.querySelector(".action")
-function checkedValue(){
-    let radio = document.getElementsByName("math")
-for(let i = 0 ;i<radio.length;i++){
-    if(radio[i].checked){
-        return radio[i].value
+let action = document.querySelector(".action");
+function checkedValue() {
+  let radio = document.getElementsByName("math");
+  for (let i = 0; i < radio.length; i++) {
+    if (radio[i].checked) {
+      return radio[i].value;
     }
+  }
 }
+action.addEventListener("click", function () {
+  let x1 = Number(document.querySelector(".x1").value);
+  let x2 = Number(document.querySelector(".x2").value);
+  let result = document.querySelector(".result");
+  if(x1 == "" || x2 == ""){
+    result.value = "Hẫy nhập giá trị 2 số !";
+  }
+  else {
+  switch (checkedValue()) {
+    case 'cộng':
+        x = x1 +x2;
+      break;
+    case 'trừ':
+        x = x1 - x2;
+        break;
+    case 'nhân':
+        x = x1 *x2;
+        break;
+    case 'chia':
+        x = x1 /x2;
+        break;
+    default:
+      break;
+  }
+  result.value = x;
 }
-action.addEventListener("click" , function(){
-    
-})
-let x1 = document.querySelector(".x1")
-let x2 = document.querySelector(".x2")
-
+});
