653905e630446bab9d7387f2761bcad5ea0a29ec
thien0709 <thien070904@gmail.com>
2023-08-10 14:22:47 +0700

    update js09

--- JS/js09.js
+++ JS/js09.js
@@ -1,12 +1,13 @@
 let create = document.querySelector("#select button:nth-child(1)");
 let tang = document.querySelector("#select button:nth-child(2)");
 let giam = document.querySelector("#select button:nth-child(3)");
-let arr = [];
+
 
 function randomList(max) {
   return Math.floor(Math.random() * max);
 }
 create.addEventListener("click", function () {
+  arr = [];
   let x = Number(document.querySelector("#container input").value);
   result = "";
   for (let i = 0; i < x; i++) {
