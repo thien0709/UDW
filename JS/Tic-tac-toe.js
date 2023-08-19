4ec2df555cf9ae762f82003e7edf5126b76d486b
thien0709 <thien070904@gmail.com>
2023-08-19 21:37:44 +0700

    update

---
+++ JS/Tic-tac-toe.js
@@ -0,0 +1,11 @@
+let colorX = document.querySelector("#xColor")
+let colorO = document.querySelector("#oColor");
+let colorBg = document.querySelector("#bgColor");
+console.log(colorBg,colorO,colorX);
+let arr = [];
+
+// Draw
+let canvas = document.querySelector("canvas");
+let ctx = canvas.getContext("2d");
+let cellSize = canvas.width/3;
+ctx.clearRect(0,)
