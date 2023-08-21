2582b27520278dccfc7b75c81b89cfdfceadd3b0
thien0709 <thien070904@gmail.com>
2023-08-21 16:24:22 +0700

    Update

--- JS/dethi4.js
+++ JS/dethi4.js
@@ -1,27 +1,61 @@
-let randomBox = document.querySelector(".box");
+let randomBox = document.querySelectorAll(".box");
 let colors = document.querySelector("#colors input");
 let selects = document.querySelectorAll('input[type="radio"]');
 let value = "10";
+let button = document.querySelector("button");
 // Get select
-for (let i = 0; i < selects.length; i++) {
-  selects[i].addEventListener("change", function () {
-
-  });
+function getSelect() {
+  for (let i = 0; i < selects.length; i++) {
+    selects[i].addEventListener("change", function () {
+      value = selects[i].value;
+    });
+  }
 }
+// Button
+button.addEventListener("click", function () {
+  getSelect();
+  let x = checkSelect(value);
+  do {
+    var arrAdd = randomNumber();
+    var sumArr = sumArray(arrAdd);
+    console.log(x.min,x.max);
+  } while (sumArr < x.min || sumArr > x.max);
+  console.log(arrAdd);
+  for (let i = 0; i < randomBox.length; i++) {
+    randomBox[i].innerHTML = arrAdd[i];
+  }
+});
 
-
-function checkSelect(e){
-    let min, max;
-    if(e == '10'){
-        min = 3;
-        max = 10;
-    }
-    else {
-        min = 11;
-        max = 18;
-    }
-    return {max,min};
+// Check select
+function checkSelect(e) {
+  let min, max;
+  if (e == "10") {
+    min = 3;
+    max = 10;
+  } else {
+    min = 11;
+    max = 18;
+  }
+  return { max, min };
 }
-function randomNumber(min,max){
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+// Random
+function randomNumber() {
+  let min = 1;
+  let max = 6;
+  let arrNumber = [];
+  let a = Math.floor(Math.random() * (max - min + 1)) + min;
+  let b = Math.floor(Math.random() * (max - min + 1)) + min;
+  let c = Math.floor(Math.random() * (max - min + 1)) + min;
+  arrNumber.push(a);
+  arrNumber.push(b);
+  arrNumber.push(c);
+  return arrNumber;
+}
+// Sum
+function sumArray(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
 }
