91a0e1e76a622747c354b1e1dca38b55ba4f5841
thien0709 <thien070904@gmail.com>
2023-08-22 22:12:14 +0700

    update

--- JS/demo.js
+++ JS/demo.js
@@ -5,10 +5,10 @@ let content2 = document.querySelector("#content2");
 console.log(c1,c2)
 // Bắt sự kiện thay đổi content
 c1.addEventListener("click", function () {
-    changeContent(content1,content2);
+    changeContent(content2,content1);
 });
 c2.addEventListener("click", function () {
-    changeContent(content2,content1);
+    changeContent(content1,content2);
 });
 
 //Hàm thay đổi content
