15ff90c4d317b68257417ae7728a5f5efd282ccb
thien0709 <thien070904@gmail.com>
2023-08-23 13:55:26 +0700

    update

---
+++ JS/runningGame.js
@@ -0,0 +1,6 @@
+let turtle = document.querySelector("#img img:nth-child(1)");
+let rabbit = document.querySelector("#img img:nth-child(2)");
+let horse =document.querySelector("#img img:nth-child(3)");
+function randDom(){
+    
+}
